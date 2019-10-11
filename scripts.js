9624859921f7d2ead9a41b2047a52268a63bfd7b
megan777 <54378494+megan777@users.noreply.github.com>
2019-10-11 10:35:53 -0500

    Use bracket notation to invoke the appropriate method

--- scripts.js
+++ scripts.js
@@ -5,33 +5,28 @@
  *
  * */
 
-
-
-const calc = {
-add: function add(a, b){
-return Number(a) + Number(b);
-},
-
-const calc = {
-  add(a, b){
+const calculator = {
+  add(a, b) {
     return Number(a) + Number(b);
-},
-subtract(a, b) {
+  },
+  subtract(a, b) {
     return a - b;
-},
+  },
 
-multiply(a, b) {
-  return a * b;
-},
+  multiply(a, b) {
+    return a * b;
+  },
 
-divide(a, b) {
-  return a / b;
-},
+  divide(a, b) {
+    return a / b;
+  },
 
-remainder(a, b) {
-  return a % b;
-}
-}
+  remainder(a, b) {
+    return a % b;
+  }
+};
+
+//console.log(calculator.add(1, 2));
 
 const form = document.querySelector("form");
 const num1 = document.querySelector("#num-1");
@@ -39,22 +34,14 @@ const num2 = document.querySelector("#num-2");
 const select = document.querySelector("select");
 const output = document.querySelector("output");
 
-//Use an anonymous callback function as the 2nd arguement.
+//Use an anonymous callback function as the 2nd argument.
 form.addEventListener("submit", function(event) {
-  // Prevent the default browser behavior - DONT SUBMIT
+  // Prevent the default browser behavior - DON'T SUBMIT
   event.preventDefault();
-
-  //console.log(event.timestamp);
-
-  /*
-   * Get the values form the input fields. Pass the arguements to the function. Display results.
-   */
-
-  // console.log(add(num1.value, num2.value));
-
-  // output.textContent = add(num1.value, num2.value);
 
   // Form inputs have a property called values.
   //To get the values of form fields...use below
-  console.log(num1.value, num2.value, select.value);
+  //console.log(num1.value, num2.value, select.value);
+
+  output.textContent = calculator[select.value](num1.value, num2.value);
 });
